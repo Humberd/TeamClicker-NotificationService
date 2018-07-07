ecd7df485a8c11fb79d40d9946f63da0f4b26474
Humberd <humberd2@gmail.com>
2018-07-07 13:47:21 +0200

    Removed embedded kafka for a real cafka inside a Docker

--- notification-service/README.md
+++ notification-service/README.md
@@ -4,13 +4,15 @@
 
 ### Files
 
- 1. _src/main/resources/jwt_public_key.der_ - **public** jwt validation key
+ 1. _src/**main**/resources/jwt_public_key.der_ - **public** jwt validation key
 
 How to generate keys -> https://stackoverflow.com/a/19387517/4256929
 
 ### Environment variables
 
  1. _TC_KAFKA_URL_ - Kafka url, for example: `192.168.99.100:32769`
+ 2. _TC_MAIL_USERNAME_ - Gmail Username
+ 3. _TC_MAIL_PASSWORD_ - Gmail Password
 
 ## Deployment Testing requirements
 
@@ -23,3 +25,5 @@ How to generate keys -> https://stackoverflow.com/a/19387517/4256929
 ### Environment variables
 
  1. _TC_KAFKA_URL_ - Kafka url, for example: `192.168.99.100:32769`
+ 2. _TC_MAIL_USERNAME_ - Gmail Username
+ 3. _TC_MAIL_PASSWORD_ - Gmail Password
--- notification-service/src/test/kotlin/com/teamclicker/notificationservice/kafka/listeners/MailListenerTest.kt
+++ notification-service/src/test/kotlin/com/teamclicker/notificationservice/kafka/listeners/MailListenerTest.kt
@@ -5,17 +5,14 @@ import com.teamclicker.notificationservice.kafka.dto.PasswordResetEmailKDTO
 import com.teamclicker.notificationservice.mail.MailService
 import com.teamclicker.notificationservice.testConfig.extensions.waitForCall
 import mu.KLogging
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.kafka.config.KafkaListenerEndpointRegistry
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.test.rule.KafkaEmbedded
-import org.springframework.kafka.test.utils.ContainerTestUtils
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.concurrent.TimeUnit
 
@@ -23,35 +20,17 @@ import java.util.concurrent.TimeUnit
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@DirtiesContext
+@DirtiesContext
 internal class MailListenerTest {
     @MockBean
     lateinit var mailService: MailService
 
     @Autowired
-    lateinit var kafkaEmbedded: KafkaEmbedded
-
-    @Autowired
-    lateinit var kafkaListenerEndpointRegistry: KafkaListenerEndpointRegistry
-
-    @Autowired
     lateinit var template: KafkaTemplate<String, Any>
 
-    @BeforeAll
-    fun setUp() {
-        // wait until the partitions are assigned
-        for (messageListenerContainer in kafkaListenerEndpointRegistry
-            .getListenerContainers()) {
-            ContainerTestUtils.waitForAssignment(
-                messageListenerContainer,
-                kafkaEmbedded.getPartitionsPerTopic()
-            )
-        }
-    }
-
     @Test
-    fun `should receive`() {
-        val data = PasswordResetEmailKDTO("humberd2@gmail.com", "hello world")
+    fun `should send passwordReset email on passwordReset message`() {
+        val data = PasswordResetEmailKDTO("admin@admin.com", "hello world")
         template.send(KafkaTopic.PASSWORD_RESET_EMAIL.value, data)
         logger.info { "Sending ${data.javaClass.simpleName}" }
         val latch = waitForCall(mailService.send(data))
