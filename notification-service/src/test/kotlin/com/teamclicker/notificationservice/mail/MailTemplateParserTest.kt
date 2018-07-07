bbad8d62eeb5b1809843e95d5eab601700e48f50
Humberd <humberd2@gmail.com>
2018-07-07 14:55:06 +0200

    Added test to check for log in mail template parser

--- notification-service/src/test/kotlin/com/teamclicker/notificationservice/mail/MailTemplateParserTest.kt
+++ notification-service/src/test/kotlin/com/teamclicker/notificationservice/mail/MailTemplateParserTest.kt
@@ -1,10 +1,12 @@
 package com.teamclicker.notificationservice.mail
 
 import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import kotlin.test.assertTrue
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class MailTemplateParserTest {
@@ -90,5 +92,43 @@ internal class MailTemplateParserTest {
             .build()
 
         assertEquals(template, newTemplate)
+    }
+
+    @Nested
+    inner class LogTest {
+        private val outContent = ByteArrayOutputStream()
+        private val errContent = ByteArrayOutputStream()
+        private val originalOut = System.out
+        private val originalErr = System.err
+
+        @BeforeEach
+        fun setUpStreams() {
+            System.setOut(PrintStream(outContent))
+            System.setErr(PrintStream(errContent))
+        }
+
+        @AfterEach
+        fun restoreStreams() {
+            System.setOut(originalOut)
+            System.setErr(originalErr)
+        }
+
+        @Test
+        fun `should print warn message when variable does not exist in a template`() {
+            @Language("XML")
+            val template = """
+                <mj-text>
+                    Hello [[username]]
+                </mj-text>
+            """.trimIndent()
+
+
+            val newTemplate = mailTemplateParser.parseTemplate(template)
+                .replace("age", 5)
+                .build()
+
+            assertEquals(template, newTemplate)
+            assert(outContent.toString().trim().endsWith("Variable [[age]] does not exist in a template"))
+        }
     }
 }
